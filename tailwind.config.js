cc550f5f77f193cee93486cbd7e8f9871b8c7c20
Nguyen-Son-Nam-1904 <nguyennam1392003@gmail.com>
2023-08-10 20:18:06 +0700

    a

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,44 +1,45 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./src/**/*.{js,jsx,ts,tsx}'],
-    darkMode: 'class',
-    theme: {
-        extend: {
-            colors: {
-                primary: {
-                    ['blur']: 'rgba(0,167,172,.25)',
-                    100: '#00A7AC',
-                },
-                content: {
-                    ['text']: '#595959',
-                    ['title']: '#061421',
-                    ['bg']: '#f8f8f8',
-                    ['border']: '#eee',
-                },
-                blur: {
-                    100: 'rgba(0,0,0,.6)',
-                },
-            },
-        },
-        keyframes: {
-            wiggle: {
-                '0%': { transform: 'translateX(100%)' },
-                '100%': { transform: 'translateX(0)' },
-            },
-        },
-        screens: {
-            xl: { max: '1279px' },
-            // => @media (max-width: 1279px) { ... }
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: 'class',
+  theme: {
+      extend: {
+          colors: {
+              primary: {
+                  ['blur']: 'rgba(0,167,172,.25)',
+                  100: '#00A7AC',
+              },
+              content: {
+                  ['text']: '#595959',
+                  ['title']: '#061421',
+                  ['bg']: '#f8f8f8',
+                  ['border']: '#eee',
+              },
+              blur: {
+                  100: 'rgba(0,0,0,.6)',
+              },
+          },
+      },
 
-            lg: { max: '1023px' },
-            // => @media (max-width: 1023px) { ... }
+      keyframes: {
+          wiggle: {
+              '0%': { transform: 'translateX(100%)' },
+              '100%': { transform: 'translateX(0)' },
+          },
+      },
+      screens: {
+          xl: { max: '1279px' },
+          // => @media (max-width: 1279px) { ... }
 
-            tablet: { max: '767px' },
-            // => @media (max-width: 767px) { ... }
+          lg: { max: '1023px' },
+          // => @media (max-width: 1023px) { ... }
 
-            mobile: { max: '639px' },
-            // => @media (max-width: 639px) { ... }
-        },
-    },
-    plugins: [],
+          tablet: { max: '767px' },
+          // => @media (max-width: 767px) { ... }
+
+          mobile: { max: '639px' },
+          // => @media (max-width: 639px) { ... }
+      },
+  },
+  plugins: [],
 };
