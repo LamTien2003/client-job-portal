55be537d6b0904c892c3ecb9477889b42a993077
AndrewLee2k3 <andrewleedev03@gmail.com>
2023-08-18 20:35:13 +0700

    Done Process UI

--- src/pages/user/Home/Home.tsx
+++ src/pages/user/Home/Home.tsx
@@ -2,15 +2,58 @@ import { useEffect } from 'react';
 import { toast } from 'react-toastify';
 import Banner from './components/Banner/Banner';
 import Section from './components/Section/Section';
+import Category from './components/Section/Category/Category';
+import Features from './components/Section/Features/Features';
+import Process from './components/Section/Process/Process';
 const Home = () => {
     useEffect(() => {
         toast.success('Test thông bao');
     }, []);
+    const titleCategory = (
+        <span>
+            Trending Jobs <span className="text-primary-100">Category</span>
+        </span>
+    );
+    const titleFeatured = (
+        <span>
+            Latest <span className="text-primary-100">Featured</span> Jobs
+        </span>
+    );
+
+    const titleProcess = (
+        <span>
+            JOBES Working <span className="text-primary-100">Process</span>
+        </span>
+    );
 
     return (
         <div>
             <Banner />
-            <Section />
+            <Section
+                title={titleCategory}
+                subTitle="To choose your trending job dream & to make future bright."
+                bg=""
+                right={true}
+            >
+                <Category />
+            </Section>
+            <Section
+                title={titleFeatured}
+                subTitle="To choose your trending job dream & to make future bright."
+                bg="bg-content-bg"
+                right={true}
+            >
+                <Features />
+            </Section>
+
+            <Section
+                title={titleProcess}
+                subTitle="To choose your trending job dream & to make future bright."
+                bg=""
+                right={false}
+            >
+                <Process />
+            </Section>
         </div>
     );
 };
