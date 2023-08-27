756801df643c9350cd54e051b013d9c26c0640f3
LamTien2003 <ltt209514@gmail.com>
2023-08-27 13:51:26 +0700

    Add Model type

---
+++ src/types/JoobSeeker.ts
@@ -0,0 +1,52 @@
+import JobApplication from './JobApplication';
+import User from './User';
+
+export interface Education {
+    major: string;
+    school: string;
+    date: {
+        from: Date;
+        to?: Date;
+    };
+    isLearning: boolean;
+}
+export interface Certification {
+    name: string;
+    organization: string;
+    date: {
+        from: Date;
+        to: Date;
+    };
+}
+export interface Experience {
+    position: string;
+    company: string;
+    date: {
+        from: Date;
+        to?: Date;
+    };
+    isWorking: boolean;
+}
+
+export interface Project {
+    name: string;
+    description: string;
+    url: string;
+    date: {
+        from: Date;
+        to?: Date;
+    };
+    isWorking: boolean;
+}
+type Skill = 'ReactJS' | 'NodeJS' | 'PHP' | 'Java' | 'Javascript' | 'Golang' | 'React-native' | 'Python';
+
+export default interface JobSeeker extends User {
+    introduce: string;
+    cvImage: string;
+    skills: Skill[];
+    educate: Education[];
+    certificate: Certification[];
+    experiences: Experience[];
+    projects: Project[];
+    jobHistory: JobApplication[];
+}
