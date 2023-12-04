8e84f13a40ae6c4fe1545ff40e0fd2916c380d49
AndrewLee2k3 <andrewleedev03@gmail.com>
2023-12-04 14:07:07 +0700

    Fix UI Cate in HomePage

--- src/pages/user/Home/components/Section/Category/CategoryItem.tsx
+++ src/pages/user/Home/components/Section/Category/CategoryItem.tsx
@@ -5,7 +5,7 @@ const CategoryItem = ({ category, images }: { category: Category; images: string
     return (
         <Link
             to={`/job-listing?type=${category.id}`}
-            className="flex justify-between items-center border-[2px] border-primary-blur rounded-md p-3 hover:border-primary-100  duration-300 gap-2 tb:justify-center tb:gap-5 mb:justify-center mb:gap-8"
+            className="flex justify-between h-20 items-center border-[2px] border-primary-blur rounded-md p-3 hover:border-primary-100  duration-300 gap-4 tb:justify-center tb:gap-5 mb:justify-center mb:gap-8"
         >
             <img src={images} alt={images} />
             <div className="font-semibold text-lg font-family-text">
