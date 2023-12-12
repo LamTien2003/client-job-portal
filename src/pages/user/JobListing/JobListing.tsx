803baaea85023e5f16ac0d123c25b810ef68b500
LamTien2003 <ltt209514@gmail.com>
2023-12-12 21:39:03 +0700

    fix position of pagination in job list page

--- src/pages/user/JobListing/JobListing.tsx
+++ src/pages/user/JobListing/JobListing.tsx
@@ -6,97 +6,107 @@ import { useGetJobsQuery } from '@/services/jobsApiSlice';
 import Job from '@/types/Job';
 import JobColumn from './components/JobColumn/JobColumn';
 import JobGutter from './components/JobGutter/JobGutter';
-import { ListColumn, ListGutter } from '@/components/Icons'
+import { ListColumn, ListGutter } from '@/components/Icons';
 import Skeleton from '@/components/Loading/Skeleton';
 import { useSearchParams } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 type filterObject = {
-    idCat: string,
-    salary: {min: number, max: number}
-    city: string,
-}
+    idCat: string;
+    salary: { min: number; max: number };
+    city: string;
+};
 const JobListing = () => {
     const [jobList, setJobList] = useState<Job[]>([]);
-    const [totalJob, setTotalJob] = useState<number>(0)
-    const [filter, setFilter] = useState<filterObject>({idCat: '', salary: {min: 1000000, max: 10000000}, city: ''})
+    const [totalJob, setTotalJob] = useState<number>(0);
+    const [filter, setFilter] = useState<filterObject>({
+        idCat: '',
+        salary: { min: 1000000, max: 10000000 },
+        city: '',
+    });
     const [listStyle, setListStyle] = useState<'column' | 'gutter'>('column');
-    const [page, setPage] = useState<number>(1)
+    const [page, setPage] = useState<number>(1);
 
-    const pageNumber = totalJob && (totalJob % 5 === 0) ? (totalJob / 5) : Math.floor(totalJob / 5 + 1)
-    
-    const [searchParams] = useSearchParams()
-    const q = searchParams.get('q')
+    const pageNumber = totalJob && totalJob % 5 === 0 ? totalJob / 5 : Math.floor(totalJob / 5 + 1);
+
+    const [searchParams] = useSearchParams();
+    const q = searchParams.get('q');
 
     const { data, isLoading, isError } = useGetJobsQuery(
-        (filter.idCat !== '' && (filter.city === '' || filter.city === 'allLocation')) ? {
-            q: q === null ? '' : q,
-            page,
-            limit: 5,
-            "salary[gte]": filter.salary.min,
-            "salary[lte]": filter.salary.max,
-            type: filter.idCat,
-        } : (filter.idCat !== '' && (filter.city !== '' && filter.city !== 'allLocation')) ? {
-            q: q === null ? '' : q,
-            page,
-            limit: 5,
-            "salary[gte]": filter.salary.min,
-            "salary[lte]": filter.salary.max,
-            type: filter.idCat,
-            p: filter.city,
-        } : (filter.idCat === '' && (filter.city !== '' && filter.city !== 'allLocation')) ? {
-            q: q === null ? '' : q,
-            page,
-            limit: 5,
-            "salary[gte]": filter.salary.min,
-            "salary[lte]": filter.salary.max,
-            p: filter.city,
-        } : {
-            q: q === null ? '' : q,
-            page,
-            limit: 5,
-            "salary[gte]": filter.salary.min,
-            "salary[lte]": filter.salary.max,
-        }
+        filter.idCat !== '' && (filter.city === '' || filter.city === 'allLocation')
+            ? {
+                  q: q === null ? '' : q,
+                  page,
+                  limit: 5,
+                  'salary[gte]': filter.salary.min,
+                  'salary[lte]': filter.salary.max,
+                  type: filter.idCat,
+              }
+            : filter.idCat !== '' && filter.city !== '' && filter.city !== 'allLocation'
+            ? {
+                  q: q === null ? '' : q,
+                  page,
+                  limit: 5,
+                  'salary[gte]': filter.salary.min,
+                  'salary[lte]': filter.salary.max,
+                  type: filter.idCat,
+                  p: filter.city,
+              }
+            : filter.idCat === '' && filter.city !== '' && filter.city !== 'allLocation'
+            ? {
+                  q: q === null ? '' : q,
+                  page,
+                  limit: 5,
+                  'salary[gte]': filter.salary.min,
+                  'salary[lte]': filter.salary.max,
+                  p: filter.city,
+              }
+            : {
+                  q: q === null ? '' : q,
+                  page,
+                  limit: 5,
+                  'salary[gte]': filter.salary.min,
+                  'salary[lte]': filter.salary.max,
+              },
     );
 
     const handleFilter = (filterObj: filterObject) => {
-        setFilter(filterObj)
-        setPage(1)
-    }
+        setFilter(filterObj);
+        setPage(1);
+    };
 
     const handleDecreasePage = () => {
-        if(page > 1) {
-            window.scrollTo(0, 0)
-            setPage(prev => prev - 1)
+        if (page > 1) {
+            window.scrollTo(0, 0);
+            setPage((prev) => prev - 1);
         }
-    }
+    };
 
     const handleIncreasePage = () => {
-        if(page < pageNumber) {
-            window.scrollTo(0, 0)
-            setPage(prev => prev + 1)
+        if (page < pageNumber) {
+            window.scrollTo(0, 0);
+            setPage((prev) => prev + 1);
         }
-    }
+    };
 
     useEffect(() => {
-        if(!isLoading && !isError && data?.data?.data) {
-            setJobList(data?.data?.data)
+        if (!isLoading && !isError && data?.data?.data) {
+            setJobList(data?.data?.data);
         }
-    }, [jobList, totalJob, data?.data?.data, isLoading, isError])
+    }, [jobList, totalJob, data?.data?.data, isLoading, isError]);
 
     useEffect(() => {
-        if(!isLoading && !isError && typeof data?.data?.totalItems === 'number') {
-            setTotalJob(data?.data?.totalItems)
+        if (!isLoading && !isError && typeof data?.data?.totalItems === 'number') {
+            setTotalJob(data?.data?.totalItems);
         }
-    }, [jobList, totalJob, data?.data?.data, data?.data?.totalItems, isLoading, isError])
+    }, [jobList, totalJob, data?.data?.data, data?.data?.totalItems, isLoading, isError]);
 
     useLayoutEffect(() => {
-        scrollTo(0,0)
-    }, [])
+        scrollTo(0, 0);
+    }, []);
 
-    console.log(jobList)
+    console.log(jobList);
 
     return (
         <>
@@ -106,7 +116,8 @@ const JobListing = () => {
                 <div className=" w-3/4 ml-3 mr-3 flex flex-col xl:mx-auto lg:mx-auto tb:mx-auto mb:mx-auto lg:pr-0 lg:w-10/12 tb:w-10/12 mb:w-11/12">
                     <div className=" mb-6 pl-3 pr-3 flex justify-between">
                         <p className="text-content-text font-medium pt-2 pb-2">
-                            Đang hiển thị <span className=" text-primary-100 font-semibold">{jobList.length}</span> công việc
+                            Đang hiển thị <span className=" text-primary-100 font-semibold">{jobList.length}</span> công
+                            việc
                         </p>
                         <div>
                             <button className=" mr-5 ml-7" onClick={() => setListStyle('gutter')}>
@@ -117,40 +128,66 @@ const JobListing = () => {
                             </button>
                         </div>
                     </div>
-                    <div className=' mb-10'>
-                        {isLoading && [...Array(5)].map((item, index) => <div key={index} className=' mb-7'><Skeleton />{item}</div>)}
-                        {!isLoading && !isError && jobList && listStyle === 'column' && <JobColumn data={jobList} />}
-                        {!isLoading && !isError && jobList && listStyle === 'gutter' && <JobGutter data={jobList} />}
-                        <div className=' flex justify-center'>
-                            {pageNumber !== 1 && jobList.length < 6 && <div 
-                                className={page > 1 ? 'flex justify-center items-center w-10 h-10 text-primary-100 text-lg font-semibold border-2 border-primary-100 rounded-full mr-2 ml-2 cursor-pointer' : ' flex justify-center items-center w-10 h-10 text-content-text text-lg font-semibold bg-gray-400 rounded-full mr-2 ml-2 cursor-default'} 
-                                onClick={handleDecreasePage}
-                            >
-                                <FontAwesomeIcon icon={faChevronLeft} />
-                            </div>}
-                            {pageNumber !== 1 && jobList.length < 6 && [...Array(pageNumber)].map((item, index) => (
-                                <div 
-                                    key={index} 
-                                    className={index + 1 === page ? ' flex justify-center items-center w-10 h-10 text-white text-lg font-semibold bg-primary-100 rounded-full mr-2 ml-2 cursor-default' : ' flex justify-center items-center w-10 h-10 text-primary-100 text-lg font-semibold border-2 border-primary-100 rounded-full mr-2 ml-2 cursor-pointer'} 
-                                    onClick={() => {
-                                        window.scrollTo(0, 0)
-                                        setPage(index + 1)
-                                    }}>{item}{index + 1}
+                    <div className="flex flex-col h-full justify-between mb-10">
+                        {isLoading &&
+                            [...Array(5)].map((item, index) => (
+                                <div key={index} className=" mb-7">
+                                    <Skeleton />
+                                    {item}
                                 </div>
                             ))}
-                            {pageNumber !== 1 && jobList.length < 6 && <div 
-                                className={page < pageNumber ? 'flex justify-center items-center w-10 h-10 text-primary-100 text-lg font-semibold border-2 border-primary-100 rounded-full mr-2 ml-2 cursor-pointer' : ' flex justify-center items-center w-10 h-10 text-content-text text-lg font-semibold bg-gray-400 rounded-full mr-2 ml-2 cursor-default'} 
-                                onClick={handleIncreasePage}
-                            >
-                                <FontAwesomeIcon icon={faChevronRight} />
-                            </div>}
+                        {!isLoading && !isError && jobList && listStyle === 'column' && <JobColumn data={jobList} />}
+                        {!isLoading && !isError && jobList && listStyle === 'gutter' && <JobGutter data={jobList} />}
+                        <div className=" flex justify-center">
+                            {pageNumber !== 1 && jobList.length < 6 && (
+                                <div
+                                    className={
+                                        page > 1
+                                            ? 'flex justify-center items-center w-10 h-10 text-primary-100 text-lg font-semibold border-2 border-primary-100 rounded-full mr-2 ml-2 cursor-pointer'
+                                            : ' flex justify-center items-center w-10 h-10 text-content-text text-lg font-semibold bg-gray-400 rounded-full mr-2 ml-2 cursor-default'
+                                    }
+                                    onClick={handleDecreasePage}
+                                >
+                                    <FontAwesomeIcon icon={faChevronLeft} />
+                                </div>
+                            )}
+                            {pageNumber !== 1 &&
+                                jobList.length < 6 &&
+                                [...Array(pageNumber)].map((item, index) => (
+                                    <div
+                                        key={index}
+                                        className={
+                                            index + 1 === page
+                                                ? ' flex justify-center items-center w-10 h-10 text-white text-lg font-semibold bg-primary-100 rounded-full mr-2 ml-2 cursor-default'
+                                                : ' flex justify-center items-center w-10 h-10 text-primary-100 text-lg font-semibold border-2 border-primary-100 rounded-full mr-2 ml-2 cursor-pointer'
+                                        }
+                                        onClick={() => {
+                                            window.scrollTo(0, 0);
+                                            setPage(index + 1);
+                                        }}
+                                    >
+                                        {item}
+                                        {index + 1}
+                                    </div>
+                                ))}
+                            {pageNumber !== 1 && jobList.length < 6 && (
+                                <div
+                                    className={
+                                        page < pageNumber
+                                            ? 'flex justify-center items-center w-10 h-10 text-primary-100 text-lg font-semibold border-2 border-primary-100 rounded-full mr-2 ml-2 cursor-pointer'
+                                            : ' flex justify-center items-center w-10 h-10 text-content-text text-lg font-semibold bg-gray-400 rounded-full mr-2 ml-2 cursor-default'
+                                    }
+                                    onClick={handleIncreasePage}
+                                >
+                                    <FontAwesomeIcon icon={faChevronRight} />
+                                </div>
+                            )}
                         </div>
                     </div>
                 </div>
             </div>
         </>
-    )
-        
+    );
 };
 
 export default JobListing;
